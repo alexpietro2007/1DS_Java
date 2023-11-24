a81a0932a70b3dc93bb10b419b44ac06f41b2d0a
alexPietro <alexpieetro@gmail.com>
2023-11-23 21:01:07 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package bom_diorno;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner (System.in);
+		
+		double x1, y1, x2, y2, dist;
+
+		x1 = ler.nextDouble(); y2 = ler.nextDouble();
+		y1 = ler.nextDouble(); y2 = ler.nextDouble();
+		
+		dist = ((x2-x1)*(x2-x1))+(y2*y1)
+		
+		ler.close();
+
+	}
+}
